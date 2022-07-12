276fb6ef7ba120080a080ac62412a53ec4c5be61
girishsardar44 <girishsardar44@gmail.com>
2022-07-12 12:14:20 +0200

    installed camelot, conda: tabletojson

---
+++ v2/src/geoJsonPointsClass.py
@@ -0,0 +1,15 @@
+from json import JSONEncoder
+ 
+class gjc:
+
+    def geometry(type, coordinates):
+        type = str(type)
+        coordinates = [coordinates]
+
+    def feature(geometry, properties={}):
+        type = "Feature",
+        properties = properties,
+        geometry = {
+            geometry
+        }
+
